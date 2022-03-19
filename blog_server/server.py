541da772c0093572c323707f815e4abeffddf4fd
朱帅杰 <zhushuaijie@zhushuaijiedeMacBook-Air.local>
2022-03-19 10:03:06 +0800

    update:server.py

--- blog_server/server.py
+++ blog_server/server.py
@@ -1,12 +1,5 @@
 import os
 
-# from dotenv import load_dotenv
-#
-# # 加载 env, 部署使用docker 环境变量，不上传本地环境文件
-# dotenv_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), '.env')
-# if os.path.exists(dotenv_path):
-#     load_dotenv(dotenv_path, override=True)  #  override=True: 覆写已存在的变量
-
 from app import create_app
 app = create_app(os.environ.get('FLASK_CONFIG'))
 
