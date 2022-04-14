297f216a0c08866b394bb10640c44ff079e27fea
朱帅杰 <zhushuaijie@zhushuaijiedeMacBook-Air.local>
2022-04-14 15:21:37 +0800

    style(web): 网站底部链接修改

--- blog_web/src/components/Footer/index.jsx
+++ blog_web/src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <>
       <footer className='footer'>
-        <p> © 2019-2022 <a href="">Zsj Blog</a> 版权所有 ICP证： <a href="http://www.beian.miit.gov.cn" target="_blank">豫ICP备19013573号-1</a></p>
+        <p> © 2019-2022 <a href="">Zsj Blog</a> 版权所有 ICP证： <a href="https://beian.miit.gov.cn" target="_blank">豫ICP备19013573号-1</a></p>
       </footer>
     </>
 
