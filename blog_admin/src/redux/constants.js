50e131290a3b3d5f6bdf30893e648cd9e0c8f2ff
zhushuaijie <szhu9903@gmail.com>
2022-06-08 15:15:41 +0800

    feat(admin): 添加文章分类管理页面

--- blog_admin/src/redux/constants.js
+++ blog_admin/src/redux/constants.js
@@ -5,6 +5,8 @@ export const GET_BLOG_DETAIL = 'getBlogDetail';
 export const GET_BLOG_COMMENT = 'getBlogComment';
 export const GET_BLOG_RECOMMEND = 'getBlogRemmend';
 export const GET_BLOG_TYPE = 'getBlogType';
+// 通用更新
+export const UPDATE_DATA = 'updateData';
 // 评论框
 export const UPDATE_IS_REPLAY = 'updateIsReplay';
 // 清空文章列表
--- blog_admin/src/redux/reducers/blog.js
+++ blog_admin/src/redux/reducers/blog.js
@@ -1,12 +1,15 @@
-import { GET_BLOG_COMMENT, GET_BLOG_DETAIL, GET_BLOG_LIST, GET_BLOG_RECOMMEND, UPDATE_IS_REPLAY, CLEAR_BLOG_LIST, GET_BLOG_TYPE, UPDATE_IS_OPERATE_BLOG } from '../constants'
+import { GET_BLOG_COMMENT, GET_BLOG_DETAIL, GET_BLOG_LIST, GET_BLOG_RECOMMEND, UPDATE_IS_REPLAY, CLEAR_BLOG_LIST, GET_BLOG_TYPE, UPDATE_IS_OPERATE_BLOG, UPDATE_DATA } from '../constants'
 
 const initState = {
   blogList: [],
-  blogType: [],
-  blogRecommend: [],
-  blogTotalSize: null,
-  isOperateBlog: false,
   blogDetail: {},
+  blogTotalSize: null,
+  blogType: [],
+  blogTypeDetail: {},
+  blogTypeTotalSize: null,
+  blogRecommend: [],
+  isOperateBlog: false, // 添加修改文章
+  isOperateBlogType: false, // 添加修改文章分类
   blogComment: [],
   showReplay: {
     replayId: 0,
@@ -22,7 +25,7 @@ export default function blog(preState=initState, action) {
       return {...preState, blogList: data.data, blogTotalSize: data.total_count};
     
     case GET_BLOG_TYPE:
-      return {...preState, blogType: data.data};
+      return {...preState, blogType: data.data, blogTypeTotalSize: data.total_count};
     
     case GET_BLOG_RECOMMEND:
       return {...preState, blogRecommend: data.data};
@@ -45,6 +48,9 @@ export default function blog(preState=initState, action) {
     
     case UPDATE_IS_OPERATE_BLOG:
       return {...preState, isOperateBlog: data.isOperateBlog, blogDetail: {}};
+    
+    case UPDATE_DATA:
+      return {...preState, ...data}
 
 
     default:
